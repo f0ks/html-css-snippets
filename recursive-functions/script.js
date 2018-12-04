5ff7dfd7bbbf7137e8aa4ecc22626068637e00e5
Albert Khamidullin <albert.khm@gmail.com>
2018-12-04 13:11:49 +0300

    recursive factorial

--- recursive-functions/script.js
+++ recursive-functions/script.js
@@ -1,15 +1,23 @@
-(function () {
+(() => {
     'use strict';
 
-    /*
+    const factorial = (num) => {
+
+        // termination
+        if (num < 0) return;
+
+        // base
+        if (num === 0) return 1;
+
+        // recursion
+        return num * factorial(num - 1);
+
+    };
 
 
-    */
-
-
-    console.log();
+    console.log(`factorial of 5 is ${factorial(5)}`);
 
 
 })();
 
-console.log('--------------------------------------------------------------');
+
