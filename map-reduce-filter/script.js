d17ebce9e94b4f5a66acc2b1936893babba48529
Albert Khamidullin <albert.khm@gmail.com>
2019-06-10 14:55:29 +0300

    map reduce filter combination

--- map-reduce-filter/script.js
+++ map-reduce-filter/script.js
@@ -63,6 +63,52 @@
   ].filter((pilot) => pilot.faction === 'Rebels');
   console.log('rebels', rebels);
 
+  // combination
+  const personnel = [
+    {
+      id: 5,
+      name: "Luke Skywalker",
+      pilotingScore: 98,
+      shootingScore: 56,
+      isForceUser: true,
+    },
+    {
+      id: 82,
+      name: "Sabine Wren",
+      pilotingScore: 73,
+      shootingScore: 99,
+      isForceUser: false,
+    },
+    {
+      id: 22,
+      name: "Zeb Orellios",
+      pilotingScore: 20,
+      shootingScore: 59,
+      isForceUser: false,
+    },
+    {
+      id: 15,
+      name: "Ezra Bridger",
+      pilotingScore: 43,
+      shootingScore: 67,
+      isForceUser: true,
+    },
+    {
+      id: 11,
+      name: "Caleb Dume",
+      pilotingScore: 71,
+      shootingScore: 85,
+      isForceUser: true,
+    },
+  ];
+
+  // get the total score of force users only
+
+  const totalJediScore = personnel
+    .filter(person => person.isForceUser)
+    .map(jedi => jedi.pilotingScore + jedi.shootingScore)
+    .reduce((acc, score) => acc + score, 0);
+  console.log('totalJediScore', totalJediScore);
 
 })();
 
