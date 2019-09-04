fd70c43c99b012663c49c4950f6ab2447b62d79d
Albert Khamidullin <albert.khm@gmail.com>
2019-09-04 15:51:42 +0300

    phone number segment focus (fix)

--- phone-number-segment/script.js
+++ phone-number-segment/script.js
@@ -5,12 +5,18 @@
 
         $('.phone__segment').keyup(function (e) {
             var $input = $(this);
-            if ($input.val().length === 0 && e.which === 8) {
+            if (($input.val().length === 0 || $input[0].selectionStart === 0) && e.which === 8) {
                 var prevInput = $input.prevAll('.phone__segment')[0];
-                if (prevInput) prevInput.focus();
+                if (prevInput) {
+                    prevInput.focus();
+                    var strLength = prevInput.value.length * 2;
+                    prevInput.setSelectionRange(strLength, strLength);
+                }
             } else if ($input.val().length >= parseInt($input.attr("maxlength"))) {
                 var nextInput = $input.nextAll('.phone__segment')[0];
-                if (nextInput) nextInput.focus();
+                if (nextInput) {
+                    nextInput.focus();
+                }
             }
         });
 
