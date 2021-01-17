7ab94fd1814a3da0011aebadb975fdd52c243bf1
Albert Khamidullin <albert.khm@gmail.com>
2021-01-17 14:01:18 +0300

    slick carousel vertical

---
+++ slick-carousel-vertical/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // slick carousel
+    $('.slider').slick({
+        dots: false,
+        vertical: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        verticalSwiping: true,
+    });
+});
